44c890838fc01333da5f65d01ad9c63ed7fa2808
Costas Ioannou <ioannou.dev@gmail.com>
2024-05-16 10:49:30 +0300

    refactored screens

--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -1,36 +1,4 @@
 // noinspection JSUnusedGlobalSymbols
+import { ProductDetailsScreen } from "../../react/screens/ProductDetailsScreen";
 
-import { Stack, useLocalSearchParams } from "expo-router";
-import { Button, StyleSheet, Text, View } from "react-native";
-import { observer } from "mobx-react-lite";
-import { useCreateProductDetailsScreenPresenter } from "../../presentation/ProductDetailsScreen.presenter";
-
-export default observer(function ProductScreen() {
-  const { id } = useLocalSearchParams<{ id: string }>();
-  const presenter = useCreateProductDetailsScreenPresenter(Number(id));
-
-  const onCartPress = () => presenter.addProductToCart();
-
-  return (
-    <>
-      <Stack.Screen options={{ title: presenter.product?.title }} />
-      {presenter.displayAddToCartButton && (
-        <Button
-          title={`Add To Cart: ${presenter.itemsInCart}`}
-          onPress={onCartPress}
-        />
-      )}
-      <View style={styles.container}>
-        <Text>{JSON.stringify(presenter.product, null, 2)}</Text>
-      </View>
-    </>
-  );
-});
-
-// TODO: This should be a wrapper component
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
-});
+export default ProductDetailsScreen;
