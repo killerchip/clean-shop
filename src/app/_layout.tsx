5de337b18a4ea143c00127c07f7095408724d5d7
Costas Ioannou <ioannou.dev@gmail.com>
2024-05-16 11:13:29 +0300

    setup rnpaper

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,12 +4,13 @@ import "reflect-metadata";
 import "../config/mobx";
 import { StatusBar } from "expo-status-bar";
 import { Stack } from "expo-router";
+import { PaperProvider } from "react-native-paper";
 
 export default function RootLayout() {
   return (
-    <>
+    <PaperProvider>
       <Stack />
       <StatusBar style="auto" />
-    </>
+    </PaperProvider>
   );
 }
